9741f7106b9c44b2327898fb2b4ca5781439b32a
zhouwei <1012018558@qq.com>
2017-11-20 17:54:35 +0800

    add log

---
+++ app/src/main/java/com/rviewtech/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.rviewtech.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.myBtn);
+
+        pressBtn(btn);
+
+        Log.i("zhouwei test","2017");
+    }
+
+    private void pressBtn(Button btn) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity();
+                finish();
+            }
+        });
+    }
+
+    private void startActivity() {
+        Intent intent = new Intent("com.rviewtech.helloword.intent.action.MainActivity");
+        startActivity(intent);
+        Toast.makeText(this,"startActivity",Toast.LENGTH_SHORT).show();
+    }
+}
