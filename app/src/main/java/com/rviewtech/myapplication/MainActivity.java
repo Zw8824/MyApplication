601e183ba8f95459918d43ec4486ac67b6f08c9a
zhouwei <1012018558@qq.com>
2017-11-21 11:49:52 +0800

    delete log

--- app/src/main/java/com/rviewtech/myapplication/MainActivity.java
+++ app/src/main/java/com/rviewtech/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
         Button btn = (Button) findViewById(R.id.myBtn);
 
         pressBtn(btn);
-
-        Log.i("zhouwei test","2017");
     }
 
     private void pressBtn(Button btn) {
